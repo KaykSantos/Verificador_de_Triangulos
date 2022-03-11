0c304b79e31f7f353625bbc7e8854d8e0b1d3742
KaykSantos <kaykarrudasantos2018@gmail.com>
2022-03-11 09:20:36 +0900

    Atualização 2.0 - testes - sem resolução

--- www/components/script.js
+++ www/components/script.js
@@ -1,24 +1,22 @@
 //função que é executada no carregamento da pagina
 window.onload = function(){
-   const medida1 = document.querySelector("#medida1");
-   const medida2 = document.querySelector("#medida2");
-   const medida3 = document.querySelector("#medida3");
+
+   let medida1 = parseFloat(document.querySelector("#medida1"));
+   let medida2 = parseFloat(document.querySelector("#medida2"));
+   let medida3 = parseFloat(document.querySelector("#medida3"));
 
    const verificar = document.querySelector("#verificar");
    verificar.addEventListener("click", function(){
        
-       if(medida1 < (medida2 + medida3) && medida2 < (medida1+medida3) && medida3 < (medida1 + medida2)){
-           if(medida1 == medida2 && medida2 == medida3){
-               document.querySelector("#result").value = "O triângulo é equilátero";              
-           }
-           if(medida1 == medida2 || medida2 == medida3 || medida3 == medida1){
-               document.querySelector("#result").value = "O triângulo é isósceles";
-           }
-           if(medida1 != medida2 && medida2 != medida3 && medida3 != medida1){
-               document.querySelector("#result").value = "O triângulo é escaleno";
-           }
-       }else{
+       if(medida1 > (medida2 + medida3) || medida2 > (medida1 + medida3) || medida3 > (medida2 + medida1)){
            document.querySelector("#result").value = "O triângulo é inválido";
        }
+       if(medida1 == medida2 && medida2 == medida3){
+           document.querySelector("#result").value = "O triângulo é equilátero";
+       }
+       if((medida1 == medida2 && medida2 != medida3) || (medida2 == medida3 && medida2 != medida1) || (medida3 == medida1 && medida3 != medida1)){
+           document.querySelector("#result").value = "O triângulo é isósceles";
+       }
+   
    });
 }
