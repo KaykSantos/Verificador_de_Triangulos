046eee41656c0d732684d5d015e2954ee4f5f21d
KaykSantos <kaykarrudasantos2018@gmail.com>
2022-03-13 00:41:30 +0900

    Salvando alterações - bug ao imprimir o result

--- www/components/script.js
+++ www/components/script.js
@@ -1,21 +1,24 @@
-//função que é executada no carregamento da pagina
 window.onload = function(){
 
-   let medida1 = parseFloat(document.querySelector("#medida1"));
-   let medida2 = parseFloat(document.querySelector("#medida2"));
-   let medida3 = parseFloat(document.querySelector("#medida3"));
+   const medida1 = document.querySelector("#medida1");
+   const medida2 = document.querySelector("#medida2");
+   const medida3 = document.querySelector("#medida3");
 
    const verificar = document.querySelector("#verificar");
    verificar.addEventListener("click", function(){
        
-       if(medida1 > (medida2 + medida3) || medida2 > (medida1 + medida3) || medida3 > (medida2 + medida1)){
-           document.querySelector("#result").value = "O triângulo é inválido";
-       }
-       if(medida1 == medida2 && medida2 == medida3){
-           document.querySelector("#result").value = "O triângulo é equilátero";
-       }
-       if((medida1 == medida2 && medida2 != medida3) || (medida2 == medida3 && medida2 != medida1) || (medida3 == medida1 && medida3 != medida1)){
-           document.querySelector("#result").value = "O triângulo é isósceles";
+       if((medida1 < medida2 + medida3) && (medida2 < medida1 + medida3) && (medida3 < medida1 + medida2)){
+           if((medida1 == medida2) && (medida2 == medida3)){
+               document.getElementById('result').value = "O triângulo é equilátero";
+           }
+           else if((medida1 == medida2 && medida2 != medida3) || (medida2 == medida3 && medida3 != medida1) || (medida3 == medida1 && medida1 != medida2)){
+               document.getElementById('result').value = "O triângulo é isósceles";
+           }
+           else{
+               document.getElementById('result').value = "O triângulo é escaleno";
+           }
+       }else{
+           document.getElementById('result').value = "O triângulo não existe";
        }
    
    });
